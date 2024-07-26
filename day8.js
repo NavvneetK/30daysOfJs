e5a0855ee452c6457147f5d475c246884ada2ed0
Navneet Kaur <navneet78141@gmail.com>
2024-07-27 00:51:02 +0530

    Create day8.js

---
+++ day8.js
@@ -0,0 +1,35 @@
+// Task 1
+let name = "Navi";
+let age = 18;
+let greeting = `My name is ${name}, ${age} years old.`;
+console.log(greeting);
+// Output: My name is Navi, 18 years old.
+
+// Task 2
+let String = `I am Navi.
+My Name is Navneet.
+I am a sophomore.`;
+console.log(String);
+// Output:
+// I am Navi.
+// My Name is Navneet.
+// I am a sophomore.
+
+// Task 3
+let numbers = [1, 2, 3, 4, 5];
+let [first, second] = numbers;
+console.log(first, second);
+// Output: 1 2
+
+// Task 4
+let book = {
+    title: "The Great Gatsby",
+    author: "F. Scott Fitzgerald",
+    year: 1925
+};
+let { title, author } = book;
+console.log(title, author);
+// Output: The Great Gatsby F. Scott Fitzgerald
+
+
+
