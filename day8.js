b3b24121afa95fe801cdd139b05fd81c957e3ad1
Navneet Kaur <navneet78141@gmail.com>
2024-07-27 01:00:39 +0530

    added day8.js

--- day8.js
+++ day8.js
@@ -23,13 +23,57 @@ console.log(first, second);
 
 // Task 4
 let book = {
-    title: "The Great Gatsby",
-    author: "F. Scott Fitzgerald",
-    year: 1925
+    title: "My Life",
+    author: "Navneet Kaur",
+    year: 2024
 };
 let { title, author } = book;
 console.log(title, author);
-// Output: The Great Gatsby F. Scott Fitzgerald
+
+// Task 5
+let oldArray = [1, 2, 3];
+let newArray = [...oldArray, 4, 5, 6];
+console.log(newArray);
+// Output: [1, 2, 3, 4, 5, 6]
+
+// Task 6
+function sum(...args) {
+    return args.reduce((acc, curr) => acc + curr, 0);
+}
+console.log(sum(1, 2, 3, 4, 5));
+// Output: 15
+
+// Task 7
+function multiply(a, b = 0) {
+    return a * b;
+}
+console.log(multiply(6, 6)); // Output: 36
+console.log(multiply(5));    // Output: 0
+
+// Task 8
+let person = {
+    name: "Navi",
+    age: 18,
+    greet() {
+        return `Hi, I am ${this.name}, ${this.age} years old.`;
+    }
+};
+console.log(person);
+console.log(person.greet());
+// Output:
+// { name: 'Navi', age: 18, greet: [Function: greet] }
+// Hi,I am Navi, 18 years old.
+
+// Task 9
+let exercise = "Yes";
+let health = {
+    name: "Navi",
+    age: 18,
+    [exercise]: "No"
+};
+console.log(health);
+// Output: { name: 'Navi', age: 18, exercise:"No" }
+
 
 
 
